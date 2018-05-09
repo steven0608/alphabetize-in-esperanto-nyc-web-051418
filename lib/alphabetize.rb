4144508aace15953aa8c538225105d99282f7407
Steven Chen <ysteven.chen2@gmail.com>
2018-05-09 01:52:07 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanton = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by do |x|
+    x.tr(esperanton,ascii)
+  end
+
 end
