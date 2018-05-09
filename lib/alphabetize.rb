9411014ddf4800ea8fa93736762f30906b0c6bbc
Steven Chen <ysteven.chen2@gmail.com>
2018-05-09 01:50:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
   # code here
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
+  arr.sort_by do |x|
+    x.tr(ESPERANTO_ALPHABET,ascii)
+  end
 end
